2a0321bfb7b24cfd3435a90c894ff83edcbf6e3c
maryam soori <m453071@gmail.com>
2021-10-31 08:06:30 +0330

    add date time

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Hekmatinasser\Verta\Verta;
+
 if (!function_exists('getImageSrc')) {
     function getImageSrc($image = '', $template = 'original')
     {
@@ -7,5 +9,13 @@ if (!function_exists('getImageSrc')) {
             return route('imagecache', ['template' => $template, 'filename' => $image]);
         }
         return null;
+    }
+}
+
+if (!function_exists('getjalaliDate')) {
+    function getjalaliDate($date)
+    {
+        $v = new Verta($date);
+        return $v->formatJalaliDate();
     }
 }
