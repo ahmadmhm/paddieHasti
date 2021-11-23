f19e622dd1313997a95f3d603c27e569b65fe915
maryam soori <m453071@gmail.com>
2021-11-23 13:28:57 +0330

    #3 fix order notification

--- app/Listeners/WasteOrder/UpdateWasteOrderHandler.php
+++ app/Listeners/WasteOrder/UpdateWasteOrderHandler.php
@@ -14,20 +14,15 @@ class UpdateWasteOrderHandler
         $waste_order = $event->waste_order;
         $status = $event->status;
 
-        if($waste_order->status_id != $status)
-        {
-            $waste_order->update([
-                'status_id' => $status
-            ]);
-
-          Notification::send(
-                $waste_order->user,
-                new UpdateWasteOrder(
-                    $waste_order,
-                    "وضعیت سفارش $waste_order->code تغییر کرد",
-                )
-            );
-        }
+        Notification::send(
+            $waste_order->user,
+            new UpdateWasteOrder(
+                $waste_order,
+                'تغییر وضعیت سفارش',
+                "وضعیت سفارش $waste_order->code تغییر کرد",
+                route('panel.orders.index')
+            )
+        );
        
 
     }
